704308163eb1daef7ae0263a2b65e0d5ea42574f
Ían Muliterno <iannimuliterno@gmail.com>
2025-08-07 18:06:49 -0300

    Update postgres_setup.sh
    creating a table to store the chat message

--- postgres_setup.sh
+++ postgres_setup.sh
@@ -11,6 +11,7 @@ createdb ian_muliterno
 psql
 
 #alter password and check users and roles
+# the >> symbol means that we are writting in psql
 >> ALTER USER ian_muliterno WITH PASSWORD '...';
 >> \du
 
@@ -21,3 +22,17 @@ psql
 
 >> create DATABASE ian_projects
 >>\q
+
+#lets go back into psql, but now connected to our db so we can create a table in it
+psql -d ian_projects
+
+>> CREATE TABLE biofield_rag_memory (
+    id SERIAL PRIMARY KEY,
+    session_id UUID NOT NULL,
+    message TEXT NOT NULL,
+    role VARCHAR(10) NOT NULL,  
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+# lets take a look at our table
+>> \d biofield_rag_memory
